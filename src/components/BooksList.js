c326874507f047e8f57a910760d04496bac1bb43
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2021-01-19 15:45:54 -0300

    fix useState

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -3,20 +3,19 @@ import Book from "./Book"
 import { Button } from "react-bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
 
+export const bookObj = [
+  {
+    id: 1,
+    title: "winds of winter",
+    category: "fantasy",
+  },
+  {
+    id: 2,
+    title: "game of thrones",
+    category: "horror",
+  },
+]
 const BooksList = () => {
-  const bookObj = [
-    {
-      id: 1,
-      title: "winds of winter",
-      category: "fantasy",
-    },
-    {
-      id: 2,
-      title: "game of thrones",
-      category: "horror",
-    },
-  ]
-
   const displayBooks = bookObj.map(book => {
     return <Book key={book.id} book={book} />
   })
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import App from "../components/App.js"
 import { Provider } from "react-redux"
-import store from "../reducers/books"
+// import store from "../reducers/books"
+import { bookObj } from "../components/BooksList"
 
-const IndexPage = () => (
-  <div>
-    <Provider store={store}>
+const IndexPage = () => {
+  const [books, setBooks] = useState(bookObj)
+  return (
+    <div>
+      {/* <Provider> */}
+      {console.log(books)}
       <App />
-    </Provider>
-  </div>
-)
+      {/* </Provider> */}
+    </div>
+  )
+}
 
 export default IndexPage
