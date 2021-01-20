de7de85c39951ad7155984d20f2f5847ebaaa8fe
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2021-01-20 12:53:25 -0300

    add form hooks

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,6 +1,14 @@
 import React from "react"
 import Form from "react-bootstrap/Form"
+import { Button } from "react-bootstrap"
+import { useForm } from "react-hook-form"
+
 const BookForm = () => {
+  const { register, handleSubmit, watch, errors } = useForm()
+  const onSubmit = data => {
+    console.log(data)
+    document.getElementById("bookinput").value = ""
+  }
   const categories = [
     "Action",
     "Biography",
@@ -14,15 +22,26 @@ const BookForm = () => {
     <option key={index}>{cat}</option>
   ))
   return (
-    <Form>
-      <Form.Group controlId="exampleForm.ControlInput1">
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Form.Group controlId="bookinput">
         <Form.Label>Book Title</Form.Label>
-        <Form.Control type="text" placeholder="Book Name" />
+        <Form.Control
+          type="text"
+          name="bookTitle"
+          placeholder="Book Title"
+          ref={register({ required: true })}
+        />
+        {errors.bookTitle && <span>This field is required</span>}
       </Form.Group>
       <Form.Group controlId="exampleForm.ControlSelect1">
         <Form.Label>Category:</Form.Label>
-        <Form.Control as="select">{categoriesOpt}</Form.Control>
+        <Form.Control as="select" name="category" ref={register}>
+          {categoriesOpt}
+        </Form.Control>
       </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
     </Form>
   )
 }
--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Book from "./Book"
-import { Button } from "react-bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
 import { bookObj } from "../pages"
 
@@ -9,11 +8,7 @@ const BooksList = () => {
     return <Book key={book.id} book={book} />
   })
 
-  return (
-    <div>
-      {displayBooks} <Button>Click me</Button>
-    </div>
-  )
+  return <div>{displayBooks}</div>
 }
 
 export default BooksList
